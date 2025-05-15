33f957c024ebf37a65d195b730795f1212290e54
João Marcelo Domingos Bruno <104376632+jmdbrunogh@users.noreply.github.com>
2025-05-15 19:13:16 -0300

    Create main.tf

---
+++ examples/dev/main.tf
@@ -0,0 +1,13 @@
+module "dev_rg" {
+  source              = "../modules/resource-group"
+  resource_group_name = "rg-dev-acme"
+  resource_group_location = "brazilsouth"
+  tags = {
+    environment = "development"
+    owner       = "devops-team"
+  }
+}
+
+output "dev_rg_id" {
+  value = module.dev_rg.resource_group_id
+}
